9989876b0300fa3fbc2b6e202217e33f9a639341
Nick Griffen <nicholas.s.griffen@gmail.com>
2018-08-22 10:04:15 -0600

    add stop party function and button

--- party.js
+++ party.js
@@ -1,12 +1,21 @@
 let musicSrc = 'https://ia802808.us.archive.org/29/items/01.OneMoreTime/01.%20One%20More%20Time.ogg'
 let boomBox = `<audio autoplay><source src=${musicSrc} type="audio/ogg"></audio>`
+let style = $("#style")
 
 function loadPartyStyle() {
-  let style = $("#style")
   style.attr('href', 'party.css')
+}
+
+function loadBusinessStyle() {
+  style.attr('href', 'site.css')
 }
 
 function startParty() {
   loadPartyStyle();
   $('main').before(boomBox)
+}
+
+function stopParty() {
+  loadBusinessStyle();
+  $('audio').remove();
 }
